44212de0ccb266aa277801ac3259e5864e7c2952
Noskcaj <kcajdev@gmail.com>
2018-01-08 20:07:59 -0500

    Filled out ui

--- index.js
+++ index.js
@@ -3,14 +3,31 @@ var ctx
 var gridWidth
 var gridHeight
 
+var moves = []
+var position
+
+function vecAdd(x, y) {
+    return [x[0] + y[0], x[1] + y[1]]
+}
+
 function drawLine(x1, y1, x2, y2) {
-    var widthMutiplier = canvas.width / gridWidth
-    var heightMutiplier = canvas.height / gridHeight
     ctx.beginPath()
 
-    ctx.moveTo(x1 * widthMutiplier, y1 * heightMutiplier)
-    ctx.lineTo(x2 * widthMutiplier, y2 * heightMutiplier)
+    moveTo(x1, y1)
+    lineTo(x2, y2)
     ctx.stroke()
+}
+
+function lineTo(x, y) {
+    let widthMutiplier = canvas.width / gridWidth
+    let heightMutiplier = canvas.height / gridHeight
+    ctx.lineTo(x * widthMutiplier, y * heightMutiplier)
+}
+
+function moveTo(x, y) {
+    let widthMutiplier = canvas.width / gridWidth
+    let heightMutiplier = canvas.height / gridHeight
+    ctx.moveTo(x * widthMutiplier, y * heightMutiplier)
 }
 
 function clear() {
@@ -32,46 +49,64 @@ function drawGrid() {
 
 
 function drawCircle(x, y, r) {
-    var widthMutiplier = canvas.width / gridWidth
-    var heightMutiplier = canvas.height / gridHeight
+    let widthMutiplier = canvas.width / gridWidth
+    let heightMutiplier = canvas.height / gridHeight
     x = x * widthMutiplier
     y = y * heightMutiplier
     ctx.moveTo(x + r, y)
     ctx.arc(x, y, r, 0, 2 * Math.PI);
-    ctx.fill()
+    // ctx.fill()
     ctx.stroke()
 }
 
 function drawTurtle() {
     ctx.fillStyle = 'blue'
     drawCircle(
-        Math.round(gridWidth / 2),
-        Math.round(gridHeight / 2),
-        5
+        Math.round(position[0]),
+        Math.round(position[1]),
+        8
     )
 }
 
+function draw() {
+    let oldWidth = ctx.lineWidth
+    ctx.lineWidth = 5
+    let oldPos = position
+    ctx.beginPath()
+    moveTo(...position)
+    for (let move of moves) {
+        position = vecAdd(position, move)
+        lineTo(...position)
+    }
+    ctx.stroke()
+    moveTo(...oldPos)
+    ctx.lineWidth = oldWidth
+}
+
 function render() {
+    position = [Math.round(gridWidth / 2), Math.round(gridHeight / 2)]
     clear()
     drawGrid()
+    draw()
     drawTurtle()
 }
 
 $(document).keydown(function (e) {
     switch (e.key) {
         case 'ArrowLeft':
-            console.log('got ArrowLeft')
+            moves.push([-1, 0])
             break
         case 'ArrowUp':
-            console.log('got ArrowUp')
+            moves.push([0, -1])
             break
         case 'ArrowRight':
-            console.log('got ArrowRight')
+            moves.push([1, 0])
             break
         case 'ArrowDown':
-            console.log('got ArrowDown')
+            moves.push([0, 1])
             break
     }
+    render()
 })
 
 
