e4db43b2b959115dd68be390de7ef486d5703731
Noskcaj <kcajdev@gmail.com>
2018-01-08 20:51:49 -0500

    Converted to external turtle module and made functional

--- index.js
+++ index.js
@@ -1,140 +1,87 @@
-var canvas
-var ctx
-var gridWidth
-var gridHeight
-
 var moves = []
-var position
-
-function vecAdd(x, y) {
-    return [x[0] + y[0], x[1] + y[1]]
-}
-
-function drawLine(x1, y1, x2, y2) {
-    ctx.beginPath()
-
-    moveTo(x1, y1)
-    lineTo(x2, y2)
-    ctx.stroke()
-}
-
-function lineTo(x, y) {
-    let widthMutiplier = canvas.width / gridWidth
-    let heightMutiplier = canvas.height / gridHeight
-    ctx.lineTo(x * widthMutiplier, y * heightMutiplier)
-}
-
-function moveTo(x, y) {
-    let widthMutiplier = canvas.width / gridWidth
-    let heightMutiplier = canvas.height / gridHeight
-    ctx.moveTo(x * widthMutiplier, y * heightMutiplier)
-}
-
-function clear() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-}
-
-function drawGrid() {
-    let oldWidth = ctx.lineWidth
-    ctx.lineWidth = .2
-    for (x = 0; x <= gridWidth; x++) {
-        for (y = 0; y <= gridHeight; y++) {
-            drawLine(x, 0, x, gridHeight)
-            drawLine(0, y, gridWidth, y)
-
-        }
-    }
-    ctx.lineWidth = oldWidth
-}
-
-
-function drawCircle(x, y, r) {
-    let widthMutiplier = canvas.width / gridWidth
-    let heightMutiplier = canvas.height / gridHeight
-    x = x * widthMutiplier
-    y = y * heightMutiplier
-    ctx.moveTo(x + r, y)
-    ctx.arc(x, y, r, 0, 2 * Math.PI);
-    // ctx.fill()
-    ctx.stroke()
-}
-
-function drawTurtle() {
-    ctx.fillStyle = 'blue'
-    drawCircle(
-        Math.round(position[0]),
-        Math.round(position[1]),
-        8
-    )
-}
-
-function draw() {
-    let oldWidth = ctx.lineWidth
-    ctx.lineWidth = 5
-    let oldPos = position
-    ctx.beginPath()
-    moveTo(...position)
-    for (let move of moves) {
-        position = vecAdd(position, move)
-        lineTo(...position)
-    }
-    ctx.stroke()
-    moveTo(...oldPos)
-    ctx.lineWidth = oldWidth
-}
-
-function render() {
-    position = [Math.round(gridWidth / 2), Math.round(gridHeight / 2)]
-    clear()
-    drawGrid()
-    draw()
-    drawTurtle()
-}
 
 $(document).keydown(function (e) {
     switch (e.key) {
         case 'ArrowLeft':
-            moves.push([-1, 0])
+            moves.push('left')
+            left(90)
             break
         case 'ArrowUp':
-            moves.push([0, -1])
+            moves.push('forward')
+            forward(15)
             break
         case 'ArrowRight':
-            moves.push([1, 0])
+            moves.push('right')
+            right(90)
             break
         case 'ArrowDown':
-            moves.push([0, 1])
+            moves.push('reverse')
+            left(180)
             break
     }
-    render()
+    codeGen()
 })
 
+$("input").on('input', codeGen)
 
-function initCanvas() {
-    canvas = document.getElementById('turtle')
-    if (canvas.getContext) {
-        ctx = canvas.getContext('2d')
+function maybePrefix(func) {
+    if ($("#api_prefix_check").is(":checked")) {
+        return $("#api_prefix_input").val() + "." + func
     } else {
-        alert('Browser not supported')
+        return func
     }
-    $(canvas).attr("width", $("#turtle").width())
-    $(canvas).attr("height", $("#turtle").width())
 }
 
-$(function () {
-    initCanvas()
+function rightApi() {
+    let right = $('#turn_right_api')
+    if (right.val() == "") {
+        return Array(3).fill(leftApi())
+    }
+    return [maybePrefix(right.val()) + "()"]
+}
 
-    $("#width").trigger('input')
-    $("#height").trigger('input')
-    render()
-})
+function leftApi() {
+    let left = $('#turn_left_api')
+    if (left.val() == "") {
+        return Array(3).fill(rightApi())
+    }
+    return [maybePrefix(left.val()) + "()"]
 
-$("#width").on('input', function () {
-    gridWidth = parseInt(this.value)
-    render()
-})
+}
 
-$("#height").on('input', function () {
-    gridHeight = parseInt(this.value)
-    render()
-})
+function forwardApi() {
+    return [maybePrefix($("#up_api").val()) + "()"]
+}
+
+function reverseApi() {
+    if ($("#down_api").val() == "") {
+        if ($('#turn_right_api').val() == "") {
+            return Array(2).fill(leftApi())
+        } else {
+            return Array(2).fill(rightApi())
+        }
+    } else {
+        return [maybePrefix($("#down_api").val() + "()")]
+    }
+}
+
+function codeGen() {
+    let lines = []
+    for (move of moves) {
+        switch (move) {
+            case 'left':
+                lines.push(...leftApi())
+                break
+            case 'right':
+                lines.push(...rightApi())
+                break
+            case 'forward':
+                lines.push(forwardApi())
+                break
+            case 'reverse':
+                lines.push(...reverseApi())
+                break
+        }
+    }
+    $("#code").text(lines.join("\n"))
+}
